dfa7c93c9e4ee560b5d3c879660153f02dddf680
nntdatkh <nntdat.kh@gmail.com>
2021-09-10 16:33:59 +0700

    add Grid, use DataContext and binding data via code-behind

---
+++ MainWindow4.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DAS_DW.DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow4.xaml
+    /// </summary>
+    public partial class DataContextSample : Window
+    {
+        public DataContextSample()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            Binding binding = new Binding("Text");
+            binding.Source = txtValue;
+            lblValue.SetBinding(TextBlock.TextProperty, binding);
+        }
+    }
+}
